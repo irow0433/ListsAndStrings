d0e53adcc489ea9bf3c4999f033bfb46d355f923
irow0433 <31513820+irow0433@users.noreply.github.com>
2017-10-03 09:32:49 -0600

    created data members for donut

--- src/useful/controller/ToolController.java
+++ src/useful/controller/ToolController.java
@@ -4,11 +4,13 @@ package useful.controller;
 import java.util.List;
 import java.util.ArrayList;
 import useful.model.Donut;
+import useful.view.PopupDisplay;
 
 public class ToolController
 {
 	//
 	private List<Donut> donutList;
+	private PopupDisplay display;
 	
 	public ToolController()
 	{
--- src/useful/view/PopupDisplay.java
+++ src/useful/view/PopupDisplay.java
@@ -1,5 +1,35 @@
 package useful.view;
 
-public class PopupDisplay {
+import javax.swing.JOptionPane;
 
+/**
+ * The popup display class for CTEC AM Session 2017
+ * @author irow0433
+ *@Version 1.0 03.10.2017
+ */
+public class PopupDisplay 
+{
+	/**
+	 * Displays the supplied message in popup windo
+	 * @param message the message that is displayed
+	 */
+	
+	public void displayText(String message)
+	{
+		JOptionPane.showMessageDialog(null, message);
+	}
+	
+	/**
+	 * Displays the suppplied question in a popup and returns the answer as a String
+	 * @param fromQuestion The question to ask the user
+	 * @return The String answer
+	 */
+	 public String getResponse(String fromQuestion)
+	{
+		String response = "";
+		
+		response += JOptionPane.showInputDialog(null, fromQuestion);
+				
+		return response;
+	}
 }
